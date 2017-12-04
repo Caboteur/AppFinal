67cdc96bc76de8e7759995e20c84c6e51fc3ed2d
BLANCHOT Adrien <ablanchot@g-a302-12.local>
2017-12-04 16:26:17 +0100

    second commit

---
+++ imports/layouts/Mainlayout.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Menu, Segment } from 'semantic-ui-react'
+
+
+
+
+
+
+
+ const Mainlayout = ({ content }) => (
+
+     <div className="main-layout">
+
+
+      {content}
+
+     </div>
+
+ );
+
+export default Mainlayout;
---
+++ imports/routes/routes.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import {FlowRouter} from 'meteor/kadira:flow-router';
+import {mount} from 'react-mounter';
+
+import Actualites from '../components/Actualites.js';
+import NewArticles from '../components/NewArticles.js';
+
+import Mainlayout from '../layouts/Mainlayout.js';
+
+FlowRouter.route('/', {
+  name: 'home',
+  action: function () {
+    mount(Mainlayout, { content: <Actualites /> });
+  },
+});
+
+
+FlowRouter.route('/NewArticles', {
+  name: 'NewArticles',
+  action: function () {
+    mount(Mainlayout, { content: <NewArticles /> });
+  },
+});
